308ffee4a581947467ef9d7984d10b8d54f1c43a
wfxey <158351052+wfxey@users.noreply.github.com>
2024-11-05 18:44:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="BetterTkinter",
-    version="1.2.0",
+    version="1.2.1",
     license="MIT",
     author="D&I Projects",
     author_email="eldritchy.help@gmail.com",
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Eldritchy/bettertkinter",
     packages=find_packages(include=["bettertkinter", "bettertkinter.*"]),
-    download_url='https://github.com/Eldritchy/bettertkinter/archive/refs/tags/v1.3.0.tar.gz',
+    download_url='https://github.com/Eldritchy/bettertkinter/archive/refs/tags/v1.2.1.tar.gz',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
